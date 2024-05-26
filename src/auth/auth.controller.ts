810cc22aa1ddd399756c49df5c3bf388afa02cd9
George <georgenico9@gmail.com>
2024-05-26 20:17:40 +0200

    feat: Add authentication controller, guard, module, and service

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,39 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { AuthGuard, RequestWithUser } from './auth.guard';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  @HttpCode(201)
+  signup(@Body() body: CreateUserDto) {
+    return this.authService.signUp(body);
+  }
+
+  @Post('login')
+  @HttpCode(200)
+  login(@Body() body: LoginDto) {
+    return this.authService.signIn(body);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('validate')
+  @HttpCode(200)
+  validateToken(@Req() request: RequestWithUser) {
+    return {
+      user: request.user,
+    };
+  }
+}
